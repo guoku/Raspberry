7347b7edb289c0265145b759e98c200422d13669
谢家欣 <edison7500@gmail.com>
2013-09-24 17:16:18 +0800

    update

---
+++ sync_code.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+find . -name '*.pyc' -exec rm -f {} \; # clean pyc 
+#rsync -avz --exclude='settings.py'  raspberry/  jiaxin@10.0.2.217:/data/www/raspberry/
+#rsync -avz --exclude='settings.py'  raspberry/  jiaxin@10.0.2.218:/data/www/raspberry/
+rsync -avz   raspberry/  jiaxin@10.0.2.217:/data/www/raspberry/
+rsync -avz   raspberry/  jiaxin@10.0.2.218:/data/www/raspberry/
