c360013d48afb40be5f76d3ab7154f986e418c61
stxiong <stxiong@stxiong-dev-machine.(none)>
2013-10-09 01:29:43 +0800

    add mobile account file

--- raspberry/mobile/urls.py
+++ raspberry/mobile/urls.py
@@ -5,9 +5,9 @@ urlpatterns = patterns('mobile.views',
     url(r'^login/$', 'login'),
     url(r'^logout/$', 'logout'),
     url(r'^register/$', 'register'),
-    url(r'^weibo/register/$', 'register_by_weibo'),
-    url(r'^weibo/bind/$', 'bind_weibo'),
-    url(r'^weibo/unbind/$', 'unbind_weibo'),
+#    url(r'^weibo/register/$', 'register_by_weibo'),
+#    url(r'^weibo/bind/$', 'bind_weibo'),
+#    url(r'^weibo/unbind/$', 'unbind_weibo'),
 )
 
 
