3ad68adebaa1e4d4d19ab043ae18ffc15bdb41b3
stxiong <stxiong@guoku.com>
2013-10-16 15:08:44 +0800

    update sync_code.sh

--- sync_code.sh
+++ sync_code.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 find . -name '*.pyc' -exec rm -f {} \; # clean pyc 
-rsync -avz --exclude='settings.py'  raspberry/  jiaxin@10.0.2.217:/data/www/raspberry/
-rsync -avz --exclude='settings.py'  raspberry/  jiaxin@10.0.2.218:/data/www/raspberry/
+#rsync -avz --exclude='settings.py'  raspberry/  jiaxin@10.0.2.217:/data/www/raspberry/
+#rsync -avz --exclude='settings.py'  raspberry/  jiaxin@10.0.2.218:/data/www/raspberry/
+#rsync -avz --exclude='settings.py'  raspberry/  stxiong@10.0.2.217:/data/www/raspberry/
+rsync -avz --exclude='settings.py'  raspberry/  stxiong@10.0.2.218:/data/www/raspberry/
 #rsync -avz   raspberry/  jiaxin@10.0.2.217:/data/www/raspberry/
 #rsync -avz   raspberry/  jiaxin@10.0.2.218:/data/www/raspberry/
