a892d0fb49a483aac57901563c9bc4e98d959e25
stxiong <stxiong@guoku.com>
2013-10-23 18:37:33 +0800

    update homepage

--- raspberry/mobile/views.py
+++ raspberry/mobile/views.py
@@ -9,8 +9,14 @@ def homepage(request):
     _rslt = {}
     _rslt['banner_list'] = [{}]
     _rslt['hot'] = []
-    _rslt['hot'].append(RBCategory(8).read())
-    _rslt['hot'].append(RBCategory(9).read())
+    _rslt['hot'].append(RBCategory(103).read())
+    _rslt['hot'].append(RBCategory(4).read())
+    _rslt['hot'].append(RBCategory(83).read())
+    _rslt['hot'].append(RBCategory(12).read())
+    _rslt['hot'].append(RBCategory(91).read())
+    _rslt['hot'].append(RBCategory(65).read())
+    _rslt['hot'].append(RBCategory(116).read())
+    _rslt['hot'].append(RBCategory(85).read())
     _rslt['hot'].append(RBCategory(10).read())
     
     return SuccessJsonResponse(_rslt)
