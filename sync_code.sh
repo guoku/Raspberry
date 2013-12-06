e2e2406b1a7dea8aef5ef4bd7a27648331827167
stxiong <stxiong@guoku.com>
2013-12-06 19:36:17 +0800

    gen freezing unselected entity script

--- sync_code.sh
+++ sync_code.sh
@@ -4,6 +4,6 @@ find . -name '*.pyc' -exec rm -f {} \; # clean pyc
 #rsync -avz --exclude='settings.py'  raspberry/  jiaxin@10.0.2.217:/data/www/raspberry/
 #rsync -avz --exclude='settings.py'  raspberry/  jiaxin@10.0.2.218:/data/www/raspberry/
 #rsync -avz --exclude='settings.py'  raspberry/  stxiong@10.0.2.217:/data/www/raspberry/
-rsync -avz --exclude='settings.py'  raspberry/  stxiong@10.0.2.218:/data/www/raspberry/
+rsync -avz --exclude='settings.py'  raspberry/  stxiong@114.113.154.47:/data/www/raspberry/
 #rsync -avz   raspberry/  jiaxin@10.0.2.217:/data/www/raspberry/
 #rsync -avz   raspberry/  jiaxin@10.0.2.218:/data/www/raspberry/
