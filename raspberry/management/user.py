3d2cdd4ef7e7957a42161e98b739f98b8b7eab42
evantre <625455600@qq.com>
2013-12-13 16:43:51 +0800

    user display

--- raspberry/management/user.py
+++ raspberry/management/user.py
@@ -14,7 +14,6 @@ import json
 from base.user import User
 from utils.paginator import Paginator
 
-
 @login_required
 def user_list(request):
     _page_num = int(request.GET.get("p", "1"))
@@ -57,7 +56,19 @@ def edit_user(request, user_id):
             },
             context_instance = RequestContext(request)
         )
+    else:
+        _username = request.POST.get("username", None)
+        _nickname = request.POST.get("nickname", None)
+        _gender = request.POST.get("gender", None)
+        _email = request.POST.get("email", None)
+        _bio = request.POST.get("bio", None)
+        _website = request.POST.get("website", None)
 
+        _user = User(user_id)
+        _user.reset_account(username=_username, email=_email)
+        _user.set_profile(_nickname, gender=_gender, bio=_bio, website=_website)
+
+        return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
 
