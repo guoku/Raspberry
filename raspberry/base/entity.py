28f90f17a0da09ab76798796f5e201d6e3e30980
stxiong <stxiong@guoku.com>
2013-12-17 21:09:00 +0800

    update

--- raspberry/base/entity.py
+++ raspberry/base/entity.py
@@ -1,26 +1,29 @@
 # coding=utf8
-from models import Entity as EntityModel
-from models import Entity_Like as EntityLikeModel
-from models import Taobao_Item_Category_Mapping as TaobaoItemCategoryMappingModel
-from models import Note as NoteModel
-from message import EntityLikeMessage, EntityNoteMessage, NoteSelectionMessage
-from models import NoteSelection
 from django.conf import settings
 from django.core.cache import cache
 from django.db.models import Count, Sum
 from mongoengine import *
-
-from category import Category 
-from note import Note
-from item import Item
-from image import Image
-from user import User 
 from utils.apns_notification import APNSWrapper
-from hashlib import md5
 import datetime
 import urllib
 import random 
 import time
+
+
+from category import Category 
+from image import Image
+from item import Item
+#from message import NoteSelectionMessage
+from models import Entity as EntityModel
+from models import Entity_Like as EntityLikeModel
+from models import Taobao_Item_Category_Mapping as TaobaoItemCategoryMappingModel
+from models import Note as NoteModel
+from models import NoteSelection
+from tasks import CreateEntityNoteMessageTask
+from note import Note
+from user import User 
+from hashlib import md5
+
 
 
 
@@ -505,7 +508,7 @@ class Entity(object):
                 
         _basic_info = self.__read_basic_info()
         if _basic_info.has_key('creator_id') and _basic_info['creator_id'] != None and _basic_info['creator_id'] != int(creator_id):
-            _message = EntityNoteMessage.create(
+            CreateEntityNoteMessageTask.delay(
                 user_id = _basic_info['creator_id'],
                 user_unread_message_count = User(_basic_info['creator_id']).get_unread_message_count(),
                 entity_id = self.entity_id,
@@ -644,3 +647,58 @@ class Entity(object):
             trackers = settings.MOGILEFS_TRACKERS 
         )
         return _datastore.get_file_data(store_key)
+
+
+    @staticmethod
+    def _available_for_selection(selected, cand):
+        i = len(selected) - 1
+        while (i > len(selected) - 3) and (i >= 0):
+            if cand['root_category_id'] == selected[i]['root_category_id']:
+                return False
+            i -= 1
+        return True
+    
+    @staticmethod
+    def arrange_entity_note_selection(select_count, start_time, interval_secs):
+        _freezing_time = datetime.datetime(2099, 1, 1) 
+        
+        _selection_cands = []
+        for _doc in NoteSelection.objects.filter(post_time__gt = _freezing_time).order_by("selected_time"):
+            _selection_cands.append({
+                'entity_id' : _doc.entity_id,
+                'note_id' : _doc.note_id,
+                'selector_id' : _doc.selector_id,
+                'selected_time' : _doc.selected_time,
+                'root_category_id' : _doc.root_category_id
+            })
+        
+        _selected = []
+        f = True
+        while f:
+            i = 0
+            f = False
+            while i < len(_selection_cands): 
+                if Entity._available_for_selection(_selected, _selection_cands[i]):
+                    _selection = _selection_cands.pop(i)
+                    _selected.append(_selection)
+                    
+                    f = True
+                    if len(_selected) >= select_count:
+                        f = False
+                        break
+                else:
+                    i += 1
+       
+        _post_time = start_time
+        for _selection in _selected:
+            Entity(_selection['entity_id']).update_note_selection_info(
+                note_id = _selection['note_id'],
+                selector_id = _selection['selector_id'],
+                selected_time = _selection['selected_time'],
+                post_time = _post_time
+            )
+            print "[%s:%s] [%d] arranged @ [%s]"%(_selection['entity_id'], _selection['note_id'], _selection['root_category_id'], _post_time)
+            _post_time += datetime.timedelta(seconds = interval_secs)
+        print "%d selection arranged in total"%len(_selected)
+    
+    
--- raspberry/base/tasks.py
+++ raspberry/base/tasks.py
@@ -4,37 +4,54 @@ from celery.task import PeriodicTask
 from celery.task.schedules import crontab
 from django.conf import settings
 
-import popularity
-import selection 
+from message import EntityNoteMessage
+#import popularity
+#import selection 
 import datetime
 import time
 
 POPULAR_ENTITY_RUN_INTERVAL_SECS = getattr(settings, 'POPULAR_ENTITY_RUN_INTERVAL_SECS', 600)
-class CalPopularEntity(PeriodicTask):
-    ignore_result = True
-    time_limit = 600
-    run_every = datetime.timedelta(seconds = POPULAR_ENTITY_RUN_INTERVAL_SECS) 
-    
-    def run(self):
-        t_start = datetime.datetime.now()
-        popularity.generate_popular_entity()
-        print "popular entity updated...%s secs cost" % str(datetime.datetime.now() - t_start)
+MAX_RETRIES = getattr(settings, 'QUEUED_REMOTE_STORAGE_RETRIES', 5)
+RETRY_DELAY = getattr(settings, 'QUEUED_REMOTE_STORAGE_RETRY_DELAY', 60)
 
-class ArrangeSelection(PeriodicTask):
+#class CalPopularEntity(PeriodicTask):
+#    ignore_result = True
+#    time_limit = 600
+#    run_every = datetime.timedelta(seconds = POPULAR_ENTITY_RUN_INTERVAL_SECS) 
+#    
+#    def run(self):
+#        t_start = datetime.datetime.now()
+#        popularity.generate_popular_entity()
+#        print "popular entity updated...%s secs cost" % str(datetime.datetime.now() - t_start)
+#
+#class ArrangeSelection(PeriodicTask):
+#    ignore_result = True
+#    time_limit = 1800
+#    run_every = crontab(minute = 0, hour = 12) #run at 2:30 am every day
+#    
+#    def run(self):
+#        _t_start = datetime.datetime.now() + datetime.timedelta(days = 1) 
+#        _year = _t_start.year
+#        _month = _t_start.month
+#        _date = _t_start.day
+#        selection.arrange( 
+#            select_count = 91,
+#            start_time = datetime.datetime(_year, _month, _date, 8, 0, 0),
+#            interval_secs = 600 
+#        )
+#        print "selection arranged...%s secs cost" % str(datetime.datetime.now() - _t_start)
+    
+class CreateEntityNoteMessageTask(Task):
     ignore_result = True
-    time_limit = 1800
-    run_every = crontab(minute = 0, hour = 12) #run at 2:30 am every day
+    time_limit = 60
+    max_retries = MAX_RETRIES
+    default_retry_delay = RETRY_DELAY
     
-    def run(self):
-        _t_start = datetime.datetime.now() + datetime.timedelta(days = 1) 
-        _year = _t_start.year
-        _month = _t_start.month
-        _date = _t_start.day
-        selection.arrange( 
-            select_count = 91,
-            start_time = datetime.datetime(_year, _month, _date, 8, 0, 0),
-            interval_secs = 600 
+    def run(self, user_id, user_unread_message_count, entity_id, note_id): 
+        _message = EntityNoteMessage.create(
+            user_id = user_id, 
+            user_unread_message_count = user_unread_message_count, 
+            entity_id = entity_id,
+            note_id = note_id
         )
-        print "selection arranged...%s secs cost" % str(datetime.datetime.now() - _t_start)
-    
 
