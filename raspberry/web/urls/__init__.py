4fc364363d62b9698e98df8de2d2ef412d1114a5
evantre <625455600@qq.com>
2013-12-25 17:39:47 +0800

    change urls

--- raspberry/web/urls/__init__.py
+++ raspberry/web/urls/__init__.py
@@ -1,15 +1,40 @@
 from django.conf.urls.defaults import url, patterns, include
 
+
 urlpatterns = patterns(
     '',
     ('^selection/$', 'web.views.main.selection'),
     (r'^popular/$', 'web.views.main.popular'),
     (r'^discover/$', 'web.views.main.discover'),
-    (r'^detail/(?P<entity_hash>\w+)/$', 'web.views.main.detail'),
-
-    ('^account/', include('web.urls.account')),
-
-    # ('^note/', include('web.urls.note')),
-    # ('^entity/', include('web.urls.entity')),
-    # ('^user/', include('web.urls.user'))
+    (r'^detail/(?P<entity_hash>\w+)/$', 'web.views.main.detail')
 )
+
+
+urlpatterns += patterns(
+    'web.views.account',
+    (r'^account/login/$', 'login'),
+    (r'^account/login/sina/$', 'login_by_sina'),
+    (r'^account/login/taobao/$', 'login_by_taobao'),
+    (r'^account/logout/$', 'logout'),
+    (r'^account/register/$', 'register'),
+    (r'^account/register/validate_nickname/$', 'is_nickname_used'),
+    (r'^account/register/validate_email/$', 'is_email_used')
+)
+
+
+# urlpatterns += patterns(
+#     'web.views.entity',
+#     (r'', '')
+# )
+#
+#
+# urlpatterns += patterns(
+#     'web.views.note',
+#     (r'', '')
+# )
+#
+#
+# urlpatterns += patterns(
+#     'web.views.user',
+#     (r'', '')
+# )
