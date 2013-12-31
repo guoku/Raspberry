3d617765c5d24ea3a6ae5157c421d6183d2f16fa
jiaxin <edison7500@gmail.com>
2013-12-31 14:40:48 +0800

    update

---
+++ raspberry/management/urls/__init__.py
@@ -0,0 +1,14 @@
+from django.conf.urls.defaults import include, url, patterns
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^category/sync/$', 'management.views.sync.sync_category', name='sync_category'),
+    url(r'^entity/create/offline/$', 'management.views.sync.create_entity_from_offline', name='create_entity_from_offline'),
+
+    url(r'^category', include('management.urls.category')),
+    url(r'^entity', include('management.urls.entity')),
+
+)
+
+__author__ = 'edison7500'
---
+++ raspberry/management/urls/entity.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import url, patterns
+
+urlpatterns = patterns(
+    'management.views.entity',
+    url(r'^$', 'entity_list', name='entity_list'),
+    url(r'^search/$', 'search_entity', name='entity_search'),
+    url(r'^new/$', 'new_entity', name='entity_new'),
+    url(r'^create/taobao/$', 'create_entity_by_taobao_item', name='create_entity_from_taobao'),
+    #url(r'^create/offline/$', 'create_entity_from_offline', name='create_entity_from_offline'),
+
+)
+
+
+
+__author__ = 'edison7500'
+
