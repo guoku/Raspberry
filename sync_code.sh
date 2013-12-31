d12ca93e2a60e430bb991e1ce21c9a426546ee74
edison7500 <edison7500@gmail.com>
2013-12-31 15:08:09 +0800

    update

--- sync_code.sh
+++ sync_code.sh
@@ -4,9 +4,3 @@ find . -name '*.pyc' -exec rm -f {} \; # clean pyc
 rsync -avz --exclude='settings.py'  raspberry/  jiaxin@114.113.154.47:/data/www/raspberry/
 #rsync -avz --exclude='settings.py'  raspberry/  jiaxin@10.0.2.218:/data/www/raspberry/
 #rsync -avz --exclude='settings.py'  raspberry/  stxiong@10.0.2.217:/data/www/raspberry/
-<<<<<<< HEAD
-=======
-rsync -avz --exclude='settings.py'  raspberry/  jiaxin@114.113.154.47:/data/www/raspberry/
-#rsync -avz   raspberry/  jiaxin@10.0.2.217:/data/www/raspberry/
-#rsync -avz   raspberry/  jiaxin@10.0.2.218:/data/www/raspberry/
->>>>>>> 72c98056fc033cda5af285852a40fecffeb4980d
