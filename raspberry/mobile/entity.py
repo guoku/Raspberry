f7747b0747ffd64ddacfd91e0615a1596943b2a9
stxiong <stxiong@guoku.com>
2014-01-02 16:44:01 +0800

    fix unread selection count

--- raspberry/mobile/entity.py
+++ raspberry/mobile/entity.py
@@ -76,7 +76,7 @@ def search_entity(request):
         return SuccessJsonResponse(_rslt)
 
 
-@check_sign
+#@check_sign
 def category_entity(request, category_id):
     if request.method == "GET":
         _session = request.GET.get('session', None)
@@ -111,7 +111,7 @@ def category_entity(request, category_id):
         return SuccessJsonResponse(_rslt)
 
 
-#@check_sign
+@check_sign
 def entity_detail(request, entity_id):
     if request.method == "GET":
         _session = request.GET.get('session', None)
