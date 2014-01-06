46c1775f02ebaf38e16853454bd4a10705872a0d
jiaxin <edison7500@gmail.com>
2014-01-06 09:59:11 +0800

    update

--- raspberry/management/urls/mobile_app.py
+++ raspberry/management/urls/mobile_app.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, patterns
 
 urlpatterns = patterns(
     'management.views.mobile_app',
-    #url(),
+    url(r'^upload/$', 'upload_file', name='management_upload_app'),
 )
 
 __author__ = 'edison7500'
--- raspberry/management/views/mobile_app.py
+++ raspberry/management/views/mobile_app.py
@@ -1,6 +1,10 @@
-from management.forms.mobile_app import UploadFileForm
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
+from management.forms.mobile_app import UploadFileForm
+
+@login_required
 def upload_file(request):
 
     if request.method == 'POST':
@@ -9,6 +13,7 @@ def upload_file(request):
     else:
         form = UploadFileForm()
         return render_to_response('mobile_app/upload_app.html',
-                                  {'form':form})
+                                  {'form': form},
+                                context_instance=RequestContext(request))
 
 __author__ = 'edison7500'
