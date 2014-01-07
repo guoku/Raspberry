d4601a9e1c870d8d4f7c79e1595511158629a431
stxiong <stxiong@guoku.com>
2014-01-07 17:38:05 +0800

    update code style

--- raspberry/management/forms/mobile_app.py
+++ raspberry/management/forms/mobile_app.py
@@ -6,8 +6,8 @@ from django import forms
 class UploadFileForm(forms.Form):
     # title = forms.CharField(max_length=150, required=False)
     file = forms.FileField(
-        label='选取一个 apk',
-        help_text='10M 以内',
+        label = '选取一个 apk',
+        help_text = '10M 以内',
     )
 
 __author__ = 'edison7500'
--- raspberry/management/models.py
+++ raspberry/management/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 
 class App_Pubilsh(models.Model):
-    name = models.CharField(max_length=150, blank=True)
-    file = models.CharField(max_length=255, blank=True)
-    is_published = models.BooleanField(default=False)
-    uploaded_time = models.DateTimeField(auto_now_add=True, editable=False)
+    name = models.CharField(max_length = 150, blank = True)
+    file = models.CharField(max_length = 255, blank = True)
+    is_published = models.BooleanField(default = False)
+    uploaded_time = models.DateTimeField(auto_now_add = True, editable = False)
 
     class Meta:
         ordering = ['-uploaded_time']
