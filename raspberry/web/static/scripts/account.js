22c7b8a7c40da34656976ac2ba53cac8e298f5b4
evantre <625455600@qq.com>
2014-01-07 12:29:49 +0800

    validate

--- raspberry/web/static/scripts/account.js
+++ raspberry/web/static/scripts/account.js
@@ -1,71 +1,67 @@
 ;(function ($) {
-  var V = {};
+  // 注册
+  var $rForm = $('form#register');
+  var $rNickName = $rForm.find('#nickname');
+  var $rEmail = $rForm.find('#email');
+  var $rPsw = $rForm.find('#psw');
+  var rV = new V();
 
-  V.checkNickname = function ($nickname, valid, remote) {
-    var nickname = $.trim($nickname.val());
-    var result;
+  $rNickName.on('change', function () {
+    rV.checkNickname($(this), true);
+  });
 
-    if (nickname.length === 0) {
-      result = '昵称不能为空';
-    } else if (nickname.length > 15) {
-      result = '昵称不能超过15个字';
-    } else if (remote) {
-      var url = '/accounts/register/check_nickname_available/';
-      $.get(url, { nickname: nickname }, function (data) {
-        var result = parseInt(data);
-        valid['nickname'] = !!result;
+  $rEmail.on('change', function () {
+    rV.checkEmail($(this), true);
+  });
 
-        if (result === 0) {
-          valid['nickname'] = false;
-          showErrMsg($nickname, '昵称已经被占用');
-        }
-      });
+  $rPsw.on('change', function () {
+    rV.checkPsw($(this));
+  });
+
+  $rForm.on('submit', function (e) {
+    if (rV.isValid()) {
+      return;
     }
 
-    valid['nickname'] = !result;
-    showErrMsg($nickname, result);
-  };
+    rV.checkNickname($rNickName, true);
+    rV.checkEmail($rEmail, true);
+    rV.checkPsw($rPsw);
 
-  V.checkEmail = function ($email,  valid, remote) {
-    var email = $.trim($email.val());
-    var result;
+    e.preventDefault()
+  });
 
-    if (email.length === 0) {
-      result = '请填写邮箱';
-    } else if (!/\S+@\S+\.\S+/.test(email)) {
-      result = '请输入正确的邮箱地址';
-    } else if (remote) {
-      var url = '/accounts/register/check_email_available/';
-      $.get(url, { email: email }, function (data) {
-        var result = parseInt(data);
-        valid['nickname'] = !!result;
+  // 登录
+  var $lForm = $('form#login');
+  var $lEmail = $lForm.find('#email');
+  var $lPsw = $lForm.find('#psw');
+  var lV = new V();
 
-        if (result === 0) {
-          showErrMsg($email, '邮箱已经被注册');
-        }
-      });
+  $lEmail.on('change', function () {
+    lV.checkEmail($(this));
+  });
+
+  $lPsw.on('change', function () {
+    lV.checkPsw($(this));
+  });
+
+  $lForm.on('submit', function (e) {
+    if (lV.isValid()) {
+      return;
     }
 
-    valid['email'] = !result;
-    showErrMsg($email, result);
-  };
+    lV.checkEmail($lEmail);
+    lV.checkPsw($lPsw);
 
-  V.checkPsw = function ($psw, valid) {
-    var psw = $.trim($psw.val());
-    var result;
-
-    if (psw.length < 6) {
-      result = '密码至少6位';
-    } else if (psw.length > 20) {
-      result = '密码最多20位';
-    }
-
-    valid['psw'] = !result;
-    showErrMsg($psw, result);
-  };
+    e.preventDefault()
+  });
 
 
-  function showErrMsg($formEle, msg) {
+  // for validation
+  function V() {
+    this.valid = [];
+  }
+
+  V.prototype._showErrMsg = function ($formEle, msg) {
     var $errorEle = $formEle.next('.error-info');
 
     if (!msg) {
@@ -75,63 +71,87 @@
     } else {
       $formEle.after('<span class="error-info">' + msg + '</span>');
     }
-  }
+  };
 
-  // 注册
-  var $rForm = $('form#register');
-  var $rNickName = $rForm.find('#nickname');
-  var $rEmail = $rForm.find('#email');
-  var $rPsw = $rForm.find('#psw');
-  var rValid = {};
+  V.prototype.checkNickname = function ($nickname, remote) {
+    var nickname = $.trim($nickname.val());
+    var result;
+    var valid = this.valid;
+    var showErrMsg = this._showErrMsg;
 
-  $rNickName.on('change', function () {
-    V.checkNickname($(this), rValid, true);
-  });
+    if (nickname.length === 0) {
+      result = '昵称不能为空';
+    } else if (nickname.length > 15) {
+      result = '昵称不能超过15个字';
+    } else if (remote) {
+      var url = '/accounts/register/check_nickname_available/';
+      $.get(url, { nickname: nickname }, function (data) {
+        var result = parseInt(data);
+        valid.push(!!result);
 
-  $rEmail.on('change', function () {
-    V.checkEmail($(this), rValid, true);
-  });
-
-  $rPsw.on('change', function () {
-    V.checkPsw($(this), rValid);
-  });
-
-  $rForm.on('submit', function (e) {
-    if (rValid.nickname && rValid.email && rValid.psw) {
-      return;
+        if (result === 0) {
+          showErrMsg($nickname, '昵称已经被占用');
+        }
+      });
     }
 
-    V.checkNickname($rNickName, rValid, true);
-    V.checkEmail($rEmail, rValid, true);
-    V.checkPsw($rPsw, rValid);
+    valid.push(!result);
+    showErrMsg($nickname, result);
+  };
 
-    e.preventDefault()
-  });
+  V.prototype.checkEmail = function ($email, remote) {
+    var email = $.trim($email.val());
+    var result;
+    var valid = this.valid;
+    var showErrMsg = this._showErrMsg;
 
-  // 登录
-  var $lForm = $('form#login');
-  var $lEmail = $lForm.find('#email');
-  var $lPsw = $lForm.find('#psw');
-  var lValid = {};
+    if (email.length === 0) {
+      result = '请填写邮箱';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      result = '请输入正确的邮箱地址';
+    } else if (remote) {
+      var url = '/accounts/register/check_email_available/';
+      $.get(url, { email: email }, function (data) {
+        var result = parseInt(data);
+        valid.push(!!result);
 
-  $lEmail.on('change', function () {
-    V.checkEmail($(this), lValid);
-  });
-
-  $lPsw.on('change', function () {
-    V.checkPsw($(this), lValid);
-  });
-
-  $lForm.on('submit', function (e) {
-    if (lValid.email && lValid.psw) {
-      return;
+        if (result === 0) {
+          showErrMsg($email, '邮箱已经被注册');
+        }
+      });
     }
 
-    V.checkEmail($lEmail, lValid);
-    V.checkPsw($lPsw, lValid);
+    valid.push(!result);
+    showErrMsg($email, result);
+  };
 
-    e.preventDefault()
-  });
+  V.prototype.checkPsw = function ($psw) {
+    var psw = $.trim($psw.val());
+    var result;
+    var valid = this.valid;
+    var showErrMsg = this._showErrMsg;
 
+    if (psw.length < 6) {
+      result = '密码至少6位';
+    } else if (psw.length > 20) {
+      result = '密码最多20位';
+    }
+
+    valid.push(!result);
+    showErrMsg($psw, result);
+  };
+
+  V.prototype.isValid = function () {
+    var len = this.valid.length;
+    if (len === 0) {
+      return false;
+    }
+
+    var ret = true;
+    for (var i = 0; i < len; i++) {
+      ret = ret && this.valid[i];
+    }
+    return ret;
+  };
 
 })(jQuery);
