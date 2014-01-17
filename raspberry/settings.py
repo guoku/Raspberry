83f15c0848608bc1f8254714d8428c0ca30077e1
stxiong <stxiong@guoku.com>
2014-01-17 17:57:12 +0800

    add os.path.join into settings

--- raspberry/settings.py
+++ raspberry/settings.py
@@ -108,8 +108,11 @@ STATIC_URL = '/static/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 BASE_DIR = os.getcwd()
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
+
 # Additional locations of static files
 STATICFILES_DIRS = (
+    ('common', os.path.join(BASE_DIR, 'static')),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
