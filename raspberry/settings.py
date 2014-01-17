8a25d5ec9f44aa8c2ec5cb0e2cc945704f666c04
stxiong <stxiong@guoku.com>
2014-01-17 17:58:31 +0800

    update

--- raspberry/settings.py
+++ raspberry/settings.py
@@ -175,6 +175,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.messages',
     'django.contrib.sessions',
     'django.contrib.staticfiles',
     'debug_toolbar',
