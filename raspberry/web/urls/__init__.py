4178d1743eecafcc5a2ac2bdfe6638bfa5225ea4
stxiong <stxiong@guoku.com>
2014-01-17 18:19:20 +0800

    update

--- raspberry/web/urls/__init__.py
+++ raspberry/web/urls/__init__.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url, patterns
 urlpatterns = patterns(
-    'web.views',
-    url(r'^selected/$', 'selected', name = "selection"),
+    'web.views.account',
     url(r'^login/$', 'login', name = "login"),
     url(r'^taobao/bind/$', 'bind_taobao', name = "bind_taobao"),
     url(r'^taobao/auth/$', 'taobao_auth', name = "taobao_auth"),
