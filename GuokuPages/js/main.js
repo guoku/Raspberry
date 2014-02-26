aee56ebfaa83f907f2363e5c9ba960c69084e56a
evantre <625455600@qq.com>
2014-02-26 20:12:42 +0800

    delete

--- GuokuPages/js/main.js
+++
@@ -1,6 +0,0 @@
-	$(function(){
-		$(".activity_item>ul").on("click",function(){
-			$(this).parents(".activity_item").toggleClass("recent_selected");
-			$(this).next(".activity_detail").toggle(200);
-		});
-	});
