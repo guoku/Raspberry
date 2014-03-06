f28f1c0dc61586dd79793caa53d46a192e36971b
stxiong <stxiong@guoku.com>
2014-03-06 19:41:32 +0800

    add tag suggest into note comment

--- raspberry/base/static/scripts/main.js
+++ raspberry/base/static/scripts/main.js
@@ -52,7 +52,7 @@
                 dom.find("p").mouseover(function(){
                     dom.find("p").removeClass("hover");
                     $(this).addClass("hover");
-                }).mousedown(function(){
+                }).click(function(){
                     var text = $(this).text().replace("# ", "");
                     var front = obj.val().slice(0, start);
                     var back = obj.val().slice(cursor);
@@ -434,6 +434,7 @@
             // 点击 为点评添加评论时候 的事件处理
 
 
+            $(".note-comment input.text").TagAC();
             var self = this;
             var $noteDetail = $noteItem.find('.note-detail');
 
@@ -569,110 +570,8 @@
                     $form.addClass('active');
                 }
             });
-            $textarea.live('keyup', function (e) {
-                var obj = $(this);
-                var e = e||window.event;
-                var code = e.which;
-                cursor = e.target.selectionEnd;
-    
-                if (code == 51 && e.shiftKey){
-                    init();
-                    start = e.target.selectionEnd;
-                    length = obj.val().length;
-    
-                    var div_text = $('<div class="text_area"></div>');
-                    obj.after(div_text);
-                    div_text.html(obj.val().slice(0,start).replace(/\n/g, "<br>").replace(/\s/g, "&nbsp;") + "<span class='pos'>&nbsp;</span>");
-    
-                    pos = div_text.find(".pos").position();
-                    
-                    getRes(obj, tag, function(){
-                        if (obj.parent().find(".tag-auto-complete").get(0) == undefined)
-                            obj.after(dom);
-                    })
-                }
-                else if (start >= 0 && length != obj.val().length && !(code > 36 && code < 41) ){
-                    clearTimeout(timeout);
-                    length = obj.val().length;
-                    tag = obj.val().slice(start, cursor);
-    
-                    timeout = setTimeout( function(){getRes(obj, tag);}, 300);
                 
-                }
-            });
-        
-            $textarea.live("keydown", function(e){
-                if (start < 0)
-                    return true;
-    
-                var obj = $(this);
-                var e = e||window.event;
-                var code = e.which;
-                cursor = e.target.selectionEnd;
-    
-                if (code == 8){
-                    if (start == cursor){
-                        init();
-                        start = -1;
-                    }
-                }
-                else if (code == 27){
-                    obj.blur();
-                }
-                else if (code == 13){
-                    if (dom.css("display") == "none")
-                        return true;
-                    var cur = dom.find("p.hover");
-                    cur.click();
-                    e.preventDefault();
-                }
-                else if (code == 38){
-                    if (dom.css("display") == "none")
-                        return true;
-    
-                    var cur = dom.find("p.hover");
-                    if (cur.get(0) == dom.find("p:first").get(0) ){
-                        dom.find("p:last").mouseover();
-                    }
-                    else {
-                        cur.prev().mouseover();
-                    }
-                    e.preventDefault();
-                }
-                else if (code == 40){
-                    if (dom.css("display") == "none")
-                        return true;
-    
-                    var cur = dom.find("p.hover");
-                    if (cur.get(0) == dom.find("p:last").get(0) ){
-                        dom.find("p:first").mouseover();
-                    }
-                    else {
-                        cur.next().mouseover();
-                    }
-                    e.preventDefault();
-                }
-                else if (code == 37){
-                    clearTimeout(timeout);
-                    if (cursor-1 < start) {
-                        dom.hide();
-                        return true;
-                    }
-                    tag = obj.val().slice(start, cursor-1);
-                    timeout = setTimeout( function(){getRes(obj, tag);}, 300);
-                }
-                else if (code == 39){
-                    clearTimeout(timeout);
-                    if (cursor+1 >= start) {
-                        tag = obj.val().slice(start, cursor+1);
-                        timeout = setTimeout( function(){getRes(obj, tag);}, 300);
-                    }
-                }
-            });
-                
-            $textarea.live("blur", function(){
-                init();
-            });
+            $textarea.TagAC();
 
             $form.find('.cancel').on('click', function () {
                 $form.removeClass('active');
