acc7d45459009839bd9d4f46dc48527b58f7bdff
jiaxin <edison7500@gmail.com>
2014-03-06 12:12:53 +0800

    update jd

--- raspberry/base/static/scripts/main.js
+++ raspberry/base/static/scripts/main.js
@@ -112,27 +112,59 @@
                     if ($this.scrollTop() > top) {
                         counter++;
                         top += 2300;
-                        var url = '/selected/?p=' + counter;
-
-                        $.get(url, function (result) {
-                            result = $.parseJSON(result);
-                            var status = parseInt(result.status);
-
-                            if (status === 1) {
-                                var $html = $(result.data);
-                                $html.each(function () {
-                                    util.showEntityTitle($(this));
-                                });
-                                $html.appendTo($selection);
-                            } else if (status === 0) {
-                                // 没有数据可以加载了
+//                        var url = '/selected/?p=' + counter;
+                        var url = window.location.href;
+//                        console.log(url);
+                        $.ajax({
+                            url: url,
+                            type: "GET",
+                            data: {'p': counter},
+                            success: function(data) {
+                                result = $.parseJSON(data);
+                                var status = parseInt(result.status);
+                                if (status == 1) {
+                                    var $html = $(result.data);
+                                    $html.each(function() {
+                                        util.showEntityTitle($(this));
+                                    });
+                                    $html.appendTo($selection);
+                                }
                             }
                         });
+//                        $.get(url, function (result) {
+//                            result = $.parseJSON(result);
+//                            var status = parseInt(result.status);
+//
+//                            if (status === 1) {
+//                                var $html = $(result.data);
+//                                $html.each(function () {
+//                                    util.showEntityTitle($(this));
+//                                });
+//                                $html.appendTo($selection);
+//                            } else if (status === 0) {
+//                                // 没有数据可以加载了
+//                            }
+//                        });
                     }
                 });
             }
         }
     };
+
+    var category = {
+        loadCategory: function () {
+            var $category = $('.category');
+            if ($category[0]) {
+                var counter = 1;
+                var top = 3000;
+
+                $(window).scroll(function () {
+                    var $this = $(this);
+//                    console.log($this);
+                });
+            }
+        }
+    }
 
     var detail = {
         updateNote: function ($noteItem) {
@@ -474,6 +506,7 @@
         util.noteHover();
 
         selection.loadSelections();
+        category.loadCategory();
 
         detail.detailImageHover();
         detail.addNote();
--- raspberry/web/views/category.py
+++ raspberry/web/views/category.py
@@ -1,4 +1,4 @@
-# from django.http import HttpResponse
+from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.utils.log import getLogger
@@ -10,11 +10,23 @@ log = getLogger('django')
 COUNT = 60
 
 def category(request, cid, template="category/category.html"):
+    if request.is_ajax():
+        if request.method == "GET":
+            _page = request.GET.get('p', 1)
+            _eids = Entity.find(
+                category_id=cid,
+                status='normal',
+                offset=_page,
+                count=COUNT,
+            )
+        return HttpResponse("OK")
+
+
     _page = request.GET.get('p', 1)
     _eids = Entity.find(
                     category_id = cid,
                     status = 'normal',
-                    offset= int(_page),
+                    offset= _page,
                     count= COUNT)
 
     entities = map(lambda x: Entity(x).read(), _eids)
