2a89f2b7d18d901a0ffc3c0419675fc24bcb647a
jiaxin <edison7500@gmail.com>
2014-03-15 18:48:51 +0800

    update

--- raspberry/web/views/__init__.py
+++ raspberry/web/views/__init__.py
@@ -5,6 +5,6 @@ from django.views.defaults import page_not_found
 
 def page_error(request):
     return server_error(request, template_name='500.html')
-    # return render_to_response('500.html')
+
 def webpage_not_found(request):
     return page_not_found(request, template_name='404.html')
