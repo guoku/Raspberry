eba275760ee8de33ca7847301f6dfdd0f9cf2988
jiaxin <edison7500@gmail.com>
2014-03-25 17:14:03 +0800

    update detail

--- raspberry/base/static/scripts/main.js
+++ raspberry/base/static/scripts/main.js
@@ -950,5 +950,28 @@ $(function(){
             $("#forget_sendmail").removeClass("btn-update").addClass("btn-disabled").attr("disabled","true");
         }
     }
+
+//    if ($.browser.msie) {
+//        $('[placeholder]').focus(function() {
+//            var input = $(this);
+//            if (input.val() == input.attr('placeholder')) {
+//                input.val('');
+//                input.removeClass('placeholder');
+//            }
+//            }).blur(function() {
+//                var input = $(this);
+//                if (input.val() == '' || input.val() == input.attr('placeholder')) {
+//                input.addClass('placeholder');
+//                input.val(input.attr('placeholder'));
+//            }
+//            }).blur().parents('form').submit(function() {
+//                $(this).find('[placeholder]').each(function() {
+//                var input = $(this);
+//                if (input.val() == input.attr('placeholder')) {
+//                input.val('');
+//            }
+//        })
+//    });
+//    }
 });
 	
