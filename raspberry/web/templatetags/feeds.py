48debd4d90f8eaf575e354a82daf54cbcf2d9e82
jiaxin <edison7500@gmail.com>
2014-03-31 15:00:26 +0800

    update

--- raspberry/web/templatetags/feeds.py
+++ raspberry/web/templatetags/feeds.py
@@ -4,6 +4,7 @@ from django import template
 from base.note import Note
 from base.entity import Entity
 from base.user import User
+# from base.item import Item, JDItem
 
 register = template.Library()
 log = getLogger('django')
@@ -14,11 +15,11 @@ def display_feed_item(obj):
     _creator_context = User(_note_context['creator_id']).read()
     _entity_id = obj.entity_id
     _entity_context = Entity(_entity_id).read()
+
     return {
         'note_context':_note_context,
         'entity_context':_entity_context,
         'creator_context':_creator_context,
-        # 'obj':obj
     }
 register.inclusion_tag("feeds/partial/note_desc.html")(display_feed_item)
 __author__ = 'edison7500'
