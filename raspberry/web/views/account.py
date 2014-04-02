c84ea09dfce9a8588b4dadf80674d92e5aa36e20
jiaxin <edison7500@gmail.com>
2014-04-02 14:09:07 +0800

    fixed download link

--- raspberry/web/views/account.py
+++ raspberry/web/views/account.py
@@ -9,8 +9,7 @@ from django.contrib.auth import authenticate
 from django.contrib.formtools.wizard.views import SessionWizardView
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-from django.contrib import messages
-import os
+
 from django.conf import settings
 # from django.core.files.storage import Storage
 from web import taobao_utils
@@ -39,41 +38,6 @@ REGISTER_TEMPLATES = {
     'register' : 'account/register.html',
     'register-bio' : 'account/register_bio.html',
 }
-
-
-# class FakeFileSystemStorage(Storage):
-#     def __init__(self, location=None, base_url=None):
-#         pass
-#
-#     def open(self, name, mode='rb'):
-#         pass
-#
-#     def save(self, name, content):
-#         pass
-#
-#     def delete(self, name):
-#         pass
-#
-#     def exists(self, name):
-#         pass
-#
-#     def listdir(self, path):
-#         pass
-#
-#     def size(self, name):
-#         pass
-#
-#     def url(self, name):
-#         pass
-#
-#     def accessed_time(self, name):
-#         pass
-#
-#     def created_time(self, name):
-#         pass
-#
-#     def modified_time(self, name):
-#         pass
 
 
 class RegisterWizard(SessionWizardView):
