9389ecb026e59baa2c455874cdb36329895667a8
jiaxin <edison7500@gmail.com>
2014-04-10 16:54:45 +0800

    update

--- raspberry/web/templatetags/entity.py
+++ raspberry/web/templatetags/entity.py
@@ -2,6 +2,7 @@
 from django.utils.log import getLogger
 from django import template
 from base.category import Category
+from base.category import Category_Group
 
 import re
 
@@ -21,9 +22,17 @@ register.inclusion_tag("entity/entity_note.html")(display_note_item)
 
 def trans_category(value):
     _category_context = Category(value).read()
+    log.info(_category_context)
     _title = _category_context['category_title'].split('-')[0]
     return _title
 register.filter('trans_category', trans_category)
+
+def group_category(value):
+    _category_context = Category(value).read()
+    _category_group = Category_Group(_category_context['group_id']).read()
+    log.info(_category_group)
+    return _category_group['title']
+register.filter('group_category', group_category)
 
 def resize_image(value, size=640):
     u = re.search("(alicdn|taobaocdn|taobao)\.com", value)
