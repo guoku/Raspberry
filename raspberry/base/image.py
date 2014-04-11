ad68789df53830b7ed423d92184cb3c020f86cf2
jiaxin <edison7500@gmail.com>
2014-04-11 14:50:57 +0800

    update

--- raspberry/base/image.py
+++ raspberry/base/image.py
@@ -5,6 +5,11 @@ from hashlib import md5
 from pymogile import Client
 from wand.image import Image as WandImage
 import datetime
+from django.utils.log import getLogger
+
+log = getLogger('django')
+
+Images_size = getattr(settings, 'IMAGES_SIZE', list())
 
 class Image(object):
     
@@ -29,7 +34,18 @@ class Image(object):
     @classmethod
     def resize(cls, data, w, h):
         _img = WandImage(blob = data)
-        _img.resize(w, h)
+        # _img.resize(w, h)
+        # return _img.make_blob()
+
+        if (w / h > _img.width / _img.height):
+            _width = round(h * _img.width / _img.height)
+            _height = h
+        else:
+            _width = w
+            _height = round(w * _img.height / _img.width)
+        _width = int(_width)
+        _height = int(_height)
+        _img.resize(_width, _height)
         return _img.make_blob()
     
     @classmethod
@@ -44,39 +60,41 @@ class Image(object):
     
     @classmethod
     def save_square_image_data_fixed(cls, store_hash, image_data):
-        _image_sizes = [240, 310, 480, 640] 
+        # _image_sizes = [64, 128, 240, 310, 480, 640]
+        # log.info(Images_size)
         _datastore = Client(
             domain = settings.MOGILEFS_DOMAIN, 
             trackers = settings.MOGILEFS_TRACKERS 
         )
-        
-        _square_data = cls.crop_square(image_data)
-        _img = WandImage(blob = _square_data)
-        if _img.width > 800 or _img.height > 800:
-            _square_data = cls.resize(_square_data, 800, 800)
+        # _square_data = cls.crop_square(image_data)
+        # _img = WandImage(blob = image_data)
+        # if _img.width > 800 or _img.height > 800:
+        #     _square_data = cls.resize(image_data, 800, 800)
         
         _fp = _datastore.new_file('img/' + store_hash + '.jpg')
-        _fp.write(_square_data)
+        _fp.write(image_data)
         _fp.close()
         
-        for _size in _image_sizes:
-            _data_resized = cls.resize(_square_data, _size, _size)
+        for _size in Images_size:
+            _data_resized = cls.resize(image_data, _size, _size)
             _fp = _datastore.new_file('img/' + store_hash + '.jpg_' + str(_size) + 'x' + str(_size) + '.jpg')
             _fp.write(_data_resized)
             _fp.close()
     
     @classmethod
     def create(cls, source, origin_url = None, image_data = None, save_in_origin = False):
+        _store_hash = None
         if origin_url != None:
             _image_id = Image.get_image_id_by_origin_url(origin_url)
-            _store_hash = None
             if _image_id != None:
                 return cls(_image_id)
         elif image_data != None:
             _store_hash = md5(image_data).hexdigest()
+
             if save_in_origin:
                 _store_hash = md5(_store_hash + 'GUOKUIMAGESAVEINORIGINSIZE').hexdigest()
                 _image_id = Image.get_image_id_by_store_hash(_store_hash)
+
                 if _image_id != None:
                     return cls(_image_id)
                 cls.save_origin_image_data(_store_hash, image_data)
@@ -86,6 +104,7 @@ class Image(object):
                 if _image_id != None:
                     return cls(_image_id)
                 cls.save_square_image_data_fixed(_store_hash, image_data)
+
         _image_obj = ImageModel( 
             source = source, 
             created_time = datetime.datetime.now(),
