4742cdac7852b7f32f7c3a9d613c015885d449c3
edison7500 <edison7500@gmail.com>
2014-09-13 00:30:34 +0800

    update

--- raspberry/settings/stage.py
+++ raspberry/settings/stage.py
@@ -104,8 +104,8 @@ MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '../uploads')
 MEDIA_URL = '/uploads/'
 # STATIC_ROOT = os.path.join(os.path.dirname(__file__), '../static/v3/')
 STATIC_ROOT = '/tmp/static/'
-STATIC_URL = 'http://static.guoku.com/static/v3/'
-# STATIC_URL = '/static/v3/'
+# STATIC_URL = 'http://static.guoku.com/static/v3/'
+STATIC_URL = '/static/v3/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
