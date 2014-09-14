e2be8c0ce801aef2f5718195ff2ec856fe25c93c
edison7500 <edison7500@gmail.com>
2014-09-14 11:19:05 +0800

    update script

--- sync_code.sh
+++ sync_code.sh
@@ -2,5 +2,6 @@
 
 find . -name '*.pyc' -exec rm -f {} \; # clean pyc 
 rsync -avz --delete raspberry/  jiaxin@114.113.154.46:/data/www/raspberry/
+rsync -avz --delete raspberry/base/static/css/ jiaxin@114.113.154.46:/data/www/core/static/v3/css/
 #rsync -avz --exclude='settings.py'  raspberry/  jiaxin@10.0.2.218:/data/www/raspberry/
 #rsync -avz --exclude='settings.py'  raspberry/  stxiong@10.0.2.217:/data/www/raspberry/
