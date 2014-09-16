f68cd64123ca0bff68816d25e3b26298bccabe05
edison7500 <edison7500@gmail.com>
2014-09-16 15:22:14 +0800

    update

--- sync_code.sh
+++ sync_code.sh
@@ -3,3 +3,4 @@
 find . -name '*.pyc' -exec rm -f {} \; # clean pyc 
 rsync -avz --delete raspberry/  jiaxin@114.113.154.46:/data/www/raspberry/
 rsync -avz --delete raspberry/base/static/css/ jiaxin@114.113.154.46:/data/www/core/static/v3/css/
+#rsync -avz --delete raspberry/base/static/font-awesome/ jiaxin@114.113.154.46:/data/www/core/static/v3/font-awesome/
