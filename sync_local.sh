33335e5492cca8c9b2a74040651aec881a49125d
edison7500 <edison7500@gmail.com>
2014-09-19 08:07:48 +0800

    update

--- sync_local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-find . -name '*.pyc' -exec rm -f {} \; # clean pyc 
-#rsync -avz --delete --exclude='settings.py' mango/  stxiong@10.0.1.23:/data/www/mango/
-rsync -avz --delete raspberry/  stxiong@10.0.1.23:/data/www/raspberry/
--- sync_pre.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-find . -name '*.pyc' -exec rm -f {} \; # clean pyc 
-#rsync -avz --delete --exclude='settings.py'   qinzhoukan/  guoku@119.167.225.32:/data/www/cupertino/
-rsync -avz --delete  raspberry/  jiaxin@114.113.154.49:/data/www/raspberry/
