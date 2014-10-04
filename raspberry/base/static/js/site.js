71e2c9606f56199ec945344404969ea5c991dce1
edison7500 <edison7500@gmail.com>
2014-10-05 00:23:37 +0800

    update

--- raspberry/base/static/js/site.js
+++ raspberry/base/static/js/site.js
@@ -17,6 +17,28 @@
             }
         },
 
+        initTag: function () {
+
+            var array = $(".with-tag");
+            for (var i=0; i<array.length; i++) {
+                var str = array.eq(i).html(array.eq(i).html().replace(/\<br[!>]*\>/g, "\n")).text();
+                if (str == undefined)
+                    continue;
+
+                var ereg = /[#＃][0-9a-zA-Z\u4e00-\u9fff\u3040-\u30FF\u30A0-\u30FF]+/g;
+                var cut = str.match(ereg);
+                if (cut == null){
+                    array.eq(i).html(str.replace(/\n/g, "<br>"));
+                    continue;
+                }
+
+                for (var j in cut){
+                    str = str.replace(cut[j], "<a class='btn-link' rel='nofollow' href='/tag/"+encodeURI(cut[j].replace(/[#＃]/,""))+"' >"+cut[j]+"</a>&nbsp;");
+                }
+
+                array.eq(i).html(str.replace(/\n/g, "<br>"));
+            }
+        },
 
         like: function () {
             // 喜爱 like entity
@@ -450,6 +472,7 @@
         util.like();
         util.follower();
         util.createEntity();
+        util.initTag();
 
         selection.loadData();
 
