5824a0a6e0bcc12464dc80b5bb871786edef9776
edison7500 <edison7500@gmail.com>
2014-10-21 22:51:32 +0800

    update

--- raspberry/management/forms/event_banner.py
+++ raspberry/management/forms/event_banner.py
@@ -1,7 +1,29 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-class CreateEventBannerForms(forms.Form):
+class BaseBannerForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        super(BaseBannerForm, self).__init__(*args, **kwargs)
+        (none, first, second, third, fourth, fifth) = (0, 1, 2, 3, 4, 5)
+        BANNER_POSITION_CHOICES = (
+            (none, _("none")),
+            (first, _("first")),
+            (second, _("second")),
+            (third, _("third")),
+            (fourth, _("fourth")),
+            (fifth, _("fifth")),
+        )
+        self.fields['position'] = forms.ChoiceField(label=_('position'),
+                                                  choices=BANNER_POSITION_CHOICES,
+                                                  widget=forms.Select(attrs={'class':'form-control',}),
+                                                  help_text=_(''))
+
+    def clean_position(self):
+        _position = self.cleaned_data.get('position')
+        return int(_position)
+
+class CreateEventBannerForms(BaseBannerForm):
 
     link = forms.URLField(
         label=_('link'),
