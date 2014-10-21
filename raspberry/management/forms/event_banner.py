e7a666c5007cf6624add57dfb0465a7f4b984c6f
edison7500 <edison7500@gmail.com>
2014-10-21 23:03:15 +0800

    update

--- raspberry/management/forms/event_banner.py
+++ raspberry/management/forms/event_banner.py
@@ -3,6 +3,17 @@ from django.utils.translation import gettext_lazy as _
 
 class BaseBannerForm(forms.Form):
 
+    link = forms.URLField(
+        label=_('link'),
+        widget=forms.TextInput(attrs={'class':'form-control'}),
+        help_text=_(''),
+    )
+    event_banner_image = forms.FileField(
+        label=_('event banner image'),
+        widget=forms.FileInput(attrs={'class':'controls'}),
+        help_text=_(''),
+    )
+
     def __init__(self, *args, **kwargs):
         super(BaseBannerForm, self).__init__(*args, **kwargs)
         (none, first, second, third, fourth, fifth) = (0, 1, 2, 3, 4, 5)
@@ -24,20 +35,56 @@ class BaseBannerForm(forms.Form):
         return int(_position)
 
 class CreateEventBannerForms(BaseBannerForm):
-
-    link = forms.URLField(
-        label=_('link'),
-        widget=forms.TextInput(attrs={'class':'form-control'}),
-        help_text=_(''),
-    )
-    event_banner_image = forms.FileField(
-        label=_('event banner image'),
-        widget=forms.FileInput(attrs={'class':'controls'}),
-        help_text=_(''),
-    )
+    #
+    # link = forms.URLField(
+    #     label=_('link'),
+    #     widget=forms.TextInput(attrs={'class':'form-control'}),
+    #     help_text=_(''),
+    # )
+    # event_banner_image = forms.FileField(
+    #     label=_('event banner image'),
+    #     widget=forms.FileInput(attrs={'class':'controls'}),
+    #     help_text=_(''),
+    # )
 
     def save(self):
 
         pass
+
+class EditEventBannerForms(BaseBannerForm):
+
+    # link = forms.URLField(
+    #     label=_('link'),
+    #     widget=forms.TextInput(attrs={'class':'form-control'}),
+    #     help_text=_(''),
+    # )
+    #
+    # event_banner_image = forms.FileField(
+    #     label=_('event banner image'),
+    #     widget=forms.FileInput(attrs={'class':'controls'}),
+    #     help_text=_(''),
+    # )
+
+    def __init__(self, banner, *args, **kwargs):
+        self.banner = banner
+        super(EditEventBannerForms, self).__init__(*args, **kwargs)
+        if self.banner.has_show_banner:
+            (none, first, second, third, fourth, fifth) = (0, 1, 2, 3, 4, 5)
+            BANNER_POSITION_CHOICES = (
+                # (none, _("none")),
+                (first, _("first")),
+                (second, _("second")),
+                (third, _("third")),
+                (fourth, _("fourth")),
+                (fifth, _("fifth")),
+            )
+            self.fields['position'] = forms.ChoiceField(label=_('position'),
+                                                  choices=BANNER_POSITION_CHOICES,
+                                                  widget=forms.Select(attrs={'class':'form-control',}),
+                                                  help_text=_(''))
+
+    def save(self):
+        pass
+
 
 __author__ = 'edison'
