021641b973ae4a93a0814ffad216c472e2b22d53
edison7500 <edison7500@gmail.com>
2014-11-07 23:15:20 +0800

    update

--- raspberry/utils/extractor/new_taobao.py
+++ raspberry/utils/extractor/new_taobao.py
@@ -6,129 +6,118 @@ from bs4 import BeautifulSoup
 import re
 from urlparse import parse_qs, urlparse
 from urllib import unquote
+from django.utils.log import getLogger
+
 
 IMG_POSTFIX = "_\d+x\d+.*\.jpg|_b\.jpg"
+log = getLogger('django')
+
+
 
 class TaoBao():
-	cookie = cookielib.CookieJar()
-	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
-	urllib2.install_opener(opener)
-	opener.addheaders.append(('Cookie','cna=I2H3CtFnDlgCAbRP3eN/4Ujy; t=2609558ec16b631c4a25eae0aad3e2dc; w_sec_step=step_login; x=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0%26__ll%3D-1%26_ato%3D0; lzstat_uv=26261492702291067296|2341454@2511607@2938535@2581747@3284827@2581759@2938538@2817407@2879138@3010391; tg=0; _cc_=URm48syIZQ%3D%3D; tracknick=; uc3=nk2=&id2=&lg2=; __utma=6906807.613088467.1388062461.1388062461.1388062461.1; __utmz=6906807.1388062461.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); mt=ci=0_0&cyk=0_0; _m_h5_tk=6457881dd2bbeba22fc0b9d54ec0f4d9_1389601777274; _m_h5_tk_enc=3c432a80ff4e2f677c6e7b8ee62bdb48; _tb_token_=uHyzMrqWeUaM; cookie2=3f01e7e62c8f3a311a6f83fb1b3456ee; wud=wud; lzstat_ss=2446520129_1_1389711010_2581747|2258142779_0_1389706922_2938535|1182737663_4_1389706953_3284827|942709971_0_1389706966_2938538|2696785043_0_1389707052_2817407|50754089_2_1389707124_2879138|2574845227_1_1389707111_3010391|377674404_1_1389711010_2581759; linezing_session=3lJ2NagSIjQvEYbpCk5o8clc_1389693042774lS4I_5; swfstore=254259; whl=-1%260%260%261389692419141; ck1=; uc1=cookie14=UoLU4ni6x8i9JA%3D%3D; v=0'))
-	
-	# images = list()
+    cookie = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+    urllib2.install_opener(opener)
+    opener.addheaders.append(('Cookie','cna=I2H3CtFnDlgCAbRP3eN/4Ujy; t=2609558ec16b631c4a25eae0aad3e2dc; w_sec_step=step_login; x=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0%26__ll%3D-1%26_ato%3D0; lzstat_uv=26261492702291067296|2341454@2511607@2938535@2581747@3284827@2581759@2938538@2817407@2879138@3010391; tg=0; _cc_=URm48syIZQ%3D%3D; tracknick=; uc3=nk2=&id2=&lg2=; __utma=6906807.613088467.1388062461.1388062461.1388062461.1; __utmz=6906807.1388062461.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); mt=ci=0_0&cyk=0_0; _m_h5_tk=6457881dd2bbeba22fc0b9d54ec0f4d9_1389601777274; _m_h5_tk_enc=3c432a80ff4e2f677c6e7b8ee62bdb48; _tb_token_=uHyzMrqWeUaM; cookie2=3f01e7e62c8f3a311a6f83fb1b3456ee; wud=wud; lzstat_ss=2446520129_1_1389711010_2581747|2258142779_0_1389706922_2938535|1182737663_4_1389706953_3284827|942709971_0_1389706966_2938538|2696785043_0_1389707052_2817407|50754089_2_1389707124_2879138|2574845227_1_1389707111_3010391|377674404_1_1389711010_2581759; linezing_session=3lJ2NagSIjQvEYbpCk5o8clc_1389693042774lS4I_5; swfstore=254259; whl=-1%260%260%261389692419141; ck1=; uc1=cookie14=UoLU4ni6x8i9JA%3D%3D; v=0'))
 
-	def __init__(self, item_id):
-		self.item_id = item_id
-		self.html = self.fetch_html()
-		self.soup = BeautifulSoup(self.html)
+    # images = list()
+
+    def __init__(self, item_id):
+        self.item_id = item_id
+        self.html = self.fetch_html()
+        self.soup = BeautifulSoup(self.html)
 
 
-	@property
-	def headers(self):
-		return self._headers
+    @property
+    def headers(self):
+        return self._headers
 
-	# @property
-	# def code(self):
-	# 	return self._code
 
-	@property
-	def nick(self):
+    @property
+    def nick(self):
 
-		self._nick = self._headers.get('at_nick')
-		if not self._nick:
-			raise
-		return unquote(self._nick)
+        self._nick = self._headers.get('at_nick')
+        if not self._nick:
+            raise
+        return unquote(self._nick)
 
-	@property
-	def cid(self):
-		cat = self.headers.get('X-Category')
-		# print cat
-		_cid = cat.split('/')
-		return _cid[-1]
+    @property
+    def cid(self):
+        cat = self.headers.get('X-Category')
+        _cid = cat.split('/')
+        return _cid[-1]
 
-	@property
-	def desc(self):
-		return self.soup.title.string[0:-4]
+    @property
+    def desc(self):
+        return self.soup.title.string[0:-4]
 
-	# @property
-	# def ptag(self):
-		# return self.soup.select("div.tb-wrap ul li strong em.tb-rmb-num")
 
-	@property
-	def price(self):
-		ptag = self.get_ptag()
-		# return ptag
-		# taobao_ptag = ptag.select("")
-		if ptag:
-			pr = ptag[0].string
-			ps = re.findall("\d+\.\d+",pr)
-			return float(ps[0])
+    @property
+    def price(self):
+        ptag = self.get_ptag()
 
-		# ptag = self.soup.select("span.originPrice")
-		# if len(ptag) > 0 :
-		# 	pr = ptag[0].string
-		# 	ps = re.findall("\d+\.\d+",pr)
-		# 	return float(ps[0])
-	@property
-	def images(self):
-		_images = list()
-		fimg = self.soup.select("img#J_ImgBooth")
-		# print fimg
-		fjpg = fimg[0].attrs.get('data-src')
-		if not fjpg:
-			fjpg = fimg[0].attrs.get('src')
+        if ptag:
+            pr = ptag[0].string
+            ps = re.findall("\d+\.\d+",pr)
+            return float(ps[0])
 
-		fjpg = re.sub(IMG_POSTFIX, "", fjpg)
-		# return fjpg
-		_images.append(fjpg)
 
-		optimgs = self.soup.select("ul#J_UlThumb li a img")
-		# print optimgs
-		for op in optimgs:
-			try:
-				optimg = re.sub(IMG_POSTFIX, "", op.attrs.get('src'))
-			except TypeError, e:
-				optimg = re.sub(IMG_POSTFIX, "", op.attrs.get('data-src'))
-			if optimg in _images:
-				continue
-			_images.append(optimg)
+    @property
+    def images(self):
+        _images = list()
+        fimg = self.soup.select("#J_ImgBooth")
 
-		return _images
+        fjpg = fimg[0].attrs.get('data-src')
+        if not fjpg:
+            fjpg = fimg[0].attrs.get('src')
 
-	@property 
-	def shoplink(self):
-		shopidtag = re.findall('shopId:"(\d+)', self.html)
-		# sl = soup.select("span.slogo a")
-		# sl = self.soup.select("span.slogo a")
-		# print sl
-		if len(shopidtag) > 0:
-			shoplink = "http://shop"+shopidtag[0]+".taobao.com"
-			return shoplink
-		return "http://chaoshi.tmall.com/"
+        fjpg = re.sub(IMG_POSTFIX, "", fjpg)
 
-	def get_ptag(self):
-		ptag = self.soup.select("div.tb-wrap ul li strong em.tb-rmb-num")
-		if len(ptag) > 0:
-			return ptag
-		ptag = self.soup.select("span.originPrice")
-		if len(ptag) > 0:
-			return ptag
+        _images.append(fjpg)
 
-		return None
+        optimgs = self.soup.select("ul#J_UlThumb li a img")
 
-	def fetch_html(self):
-		try:
-			f = self.opener.open('http://item.taobao.com/item.htm?id=%s' % self.item_id)
-		except Exception, e:
-			raise
-		
-		# self._code = f.getcode()
-		self._headers = f.headers
-		# self._html = f.read()
-		return f.read()
+        for op in optimgs:
+            try:
+                optimg = re.sub(IMG_POSTFIX, "", op.attrs.get('src'))
+            except TypeError, e:
+                optimg = re.sub(IMG_POSTFIX, "", op.attrs.get('data-src'))
+            if optimg in _images:
+                continue
+            _images.append(optimg)
 
-	def res(self):
-		result = {
+        return _images
+
+    @property
+    def shoplink(self):
+        shopidtag = re.findall('shopId:"(\d+)', self.html)
+
+        if len(shopidtag) > 0:
+            shoplink = "http://shop"+shopidtag[0]+".taobao.com"
+            return shoplink
+        return "http://chaoshi.tmall.com/"
+
+    def get_ptag(self):
+        ptag = self.soup.select("div.tb-wrap ul li strong em.tb-rmb-num")
+        if len(ptag) > 0:
+            return ptag
+        ptag = self.soup.select("span.originPrice")
+        if len(ptag) > 0:
+            return ptag
+
+        return None
+
+    def fetch_html(self):
+        try:
+            f = self.opener.open('http://item.taobao.com/item.htm?id=%s' % self.item_id)
+        except Exception, e:
+            raise
+
+        self._headers = f.headers
+        return f.read()
+
+    def res(self):
+        result = {
 			"desc": self.desc,
 			"cid": self.cid,
 			"promprice" : self.price,
@@ -140,23 +129,23 @@ class TaoBao():
 			"nick": self.nick,
 			"shop_link": self.shoplink,
 			"location": "",
-		}
-		return result
+        }
+        return result
 
 
 
 if __name__=="__main__":
-	# t = TaoBao("9960937204")
-	t = TaoBao("41253849085")
-	# print t.html
-	# t = TaoBao("36668554861")
-	print t.res()
-	# print t.nick
-	# print t.cid
-	# print t.price
-	# print t.desc
-	# print t.images
-	# print t.shoplink
+    # t = TaoBao("9960937204")
+    # t = TaoBao("39523724233")
+    # print t.soup.select("img#J_ImgBooth")
+    t = TaoBao("36668554861")
+    print t.res()
+    # print t.nick
+    # print t.cid
+    # print t.price
+    # print t.desc
+    # print t.images
+    # print t.shoplink
 
 
 
