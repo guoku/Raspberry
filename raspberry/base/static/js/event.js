6326c0ff3d2c7c4f5c173567c797e003b6336675
edison7500 <edison7500@gmail.com>
2014-12-06 11:58:35 +0800

    update

--- raspberry/base/static/js/event.js
+++ raspberry/base/static/js/event.js
@@ -2,5 +2,16 @@
  * Created by edison on 14/12/6.
  */
 ;(function ($, document, window) {
-
+//
+    var url = '/event/hongbao/';
+    $('.hongbao').on('click' ,function(e){
+        console.log('OKOKOK');
+//        $.ajax({
+//            url: url,
+//            type: 'GET',
+//            success: function(data){
+//                console.log(data.status);
+//            }
+//        });
+    });
 })(jQuery, document, window);
