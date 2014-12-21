78ef9db4b5824c1319c8cf4c4c8d6ec93c100934
edison7500 <edison7500@gmail.com>
2014-12-21 20:18:52 +0800

    update

--- raspberry/settings/static.py
+++ raspberry/settings/static.py
@@ -6,10 +6,10 @@ import os
 
 MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '../uploads')
 MEDIA_URL = '/uploads/'
-# STATIC_ROOT = os.path.join(os.path.dirname(__file__), '../static/v3/75c7991a592ba89890c046031a81a428ad0b405a/'
+# STATIC_ROOT = os.path.join(os.path.dirname(__file__), '../static/v3/3f75bb1f38e6433df42c1a1cdcad85fd29b081e3/'
 STATIC_ROOT = '/tmp/static/'
-STATIC_URL = 'http://static.guoku.com/static/v3/75c7991a592ba89890c046031a81a428ad0b405a/'
-# STATIC_URL = '/static/v3/75c7991a592ba89890c046031a81a428ad0b405a/'
+STATIC_URL = 'http://static.guoku.com/static/v3/3f75bb1f38e6433df42c1a1cdcad85fd29b081e3/'
+# STATIC_URL = '/static/v3/3f75bb1f38e6433df42c1a1cdcad85fd29b081e3/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
